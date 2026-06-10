6bc324cddcbcd7986d04cecdf66f800f1997d501
Sebastian Ramirez <sebastianramirezmarino@gmail.com>
2026-06-10 18:25:04 -0500

    added the database back

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,15 +4,20 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data';
 import{ Suspense } from 'react';
-import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+import { 
+  RevenueChartSkeleton, 
+  LatestInvoicesSkeleton,
+ } from '@/app/ui/skeletons';
  
 export default async function Page() {
+
     const { 
         numberOfCustomers, 
         numberOfInvoices, 
         totalPaidInvoices, 
         totalPendingInvoices 
     } = await fetchCardData();
+    
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
